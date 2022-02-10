92d69dfbd3b99898078730029b8f071751c4b831
BethaSL <bethasanchez3@gmail.com>
2022-02-10 16:02:12 +0000

    Fisrt commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,50 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User (Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(220), nullable=False)
+    user_id = Column(String(200), nullable=False)
+    password = Column(String(10), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    post= relationship("Post", back_populates="parent")
+    followers= relationship("Followers", back_populates="parent")
+    following= relationship("Following", back_populates="parent")
+
+class Post (Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    likes = Column(Integer, nullable=False)
+    comments = Column(String(140))
+    user_id= Column(Integer, ForeignKey("user.id"))
+
+    user= relationship("User", back_populates="children")
+
+
+class Followers (Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key=True)
+    follower_id = Column(String(200), nullable=False)
+    user_id= Column(Integer, ForeignKey("user.id"))
+
+    user= relationship("User", back_populates="children")
+
+class Following (Base):
+    __tablename__ = 'following'
+    id = Column(Integer, primary_key=True)
+    follower_id = Column(String(200), nullable=False)
+    user_id= Column(Integer, ForeignKey("user.id"))
+
+    user= relationship("User", back_populates="children")
 
     def to_dict(self):
-        return {}
+       return {}
+
+  
+
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
